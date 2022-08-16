bd000b609d126fcd948f7be3286037152405fec3
Tarek Gamal <tarek@gmail.com>
2022-08-16 21:34:01 +0200

    movies module

--- lib/movie/presentation/screens/popular_movies.dart
+++ lib/movie/presentation/screens/popular_movies.dart
@@ -52,11 +52,7 @@ class PopularMoviesScreen extends StatelessWidget {
                       releaseDate: state.popularMovies[index].releaseDate,
                       voteAverage: state.popularMovies[index].voteAverage,
                       isBottomSheet: false,
-<<<<<<< HEAD
                       color: AppColorsDark.lightBlackColor,
-=======
-                      color: AppColorsDark.darkGreyColor,
->>>>>>> 5ba464fe2721ac74d17081bb7d43ee350b399867
                     ),
                   ),
                 );
